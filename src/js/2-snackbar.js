6aaa16d593716e8844ee968b8bdf0afcd8ba46c7
Yuliia-Oliinyk777 <yuliya2474@gmail.com>
2024-08-21 21:15:37 +0200

    add homework 10

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,38 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const formEl = document.querySelector('.form');
+ const onFormSubmit = (event) => {
+  event.preventDefault();
+
+  const delay = Number(event.target.delay.value);
+  const state = event.target.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight'
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight'
+      });
+    });
+     formEl.reset();
+};
+formEl.addEventListener('submit', onFormSubmit);
